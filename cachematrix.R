67c79bceb5adc461e251bafea52dc79f6c11dbe7
Gladstone1263 <marcpacitti23@gmail.com>
2015-04-24 21:17:10 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,32 @@
-## Put comments here that give an overall description of what your
-
 ## This function creates a list with functions that set the value of the matrix and the inverse
 ## and get the value of the matrix and inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-  x<<-y
-  m<<-NULL
+   makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y){
+      x <<- y
+      m <<- NULL
 }
-get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
+      get <- function() x
+      setmatrix <- function(solve) m<<- solve
+      getmatrix <- function() m
+      list(set=set, get=get,
+           setmatrix=setmatrix,
+           getmatrix=getmatrix)
 }
 
 
 ## This function checks the cache and returns the inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m<-x$getmatrix()
-    if(!is.null(m)){
+   cacheSolve <- function(x, ...) {
+      ## Return a matrix that is the inverse of 'x'
+     m <- x$getmatrix()
+     if(!is.null(m)){
       message("getting cached data")
       return(m)
-    }
-    matrix<-x$get()
-    m<-solve(matrix, ...)
+  }
+    matrix <- x$get()
+    m <- solve(matrix, ...)
     x$setmatrix(m)
-    m
+      m
 }
